d656530633b24e3d941955ab1334b333dfa847ed
Akira Kido <achts.y@gmail.com>
2017-06-05 13:14:02 +0900

    Fixed output directory

--- Revn/Program.cs
+++ Revn/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 using System.IO;
 using System.Reflection;
 using RevnCompiler;
@@ -9,8 +10,12 @@ namespace Revn
     {
         static void Main(string[] args)
         {
-            var program =
-                File.ReadAllText( @"C:\Users\ichi-dohi\Documents\Visual Studio 2015\Projects\Revn\Revn\HelloWorld.rv" );
+            
+            var location = Path.GetDirectoryName( Assembly.GetEntryAssembly().Location );
+            var sourceCodePath = Path.Combine( location, "HelloWorld.rv" );
+            var program = File.ReadAllText(sourceCodePath);
+
+
             var lexer = new Lexer(program);
             var tokens = lexer.GenerateTokens();
             foreach(var token in tokens)
@@ -20,7 +25,8 @@ namespace Revn
 
             Parser parser = new Parser(tokens);
             var result = parser.Parse();
-            FileStream file = File.Create(@"C:\Users\ichi-dohi\Desktop\hoge.il");
+            var ilPath = Path.Combine( location, "HelloWorld.il" );
+            FileStream file = File.Create(ilPath);
             using ( var writer = new StreamWriter( file ) )
             {
                 writer.WriteLine(".assembly extern mscorlib { }\n.assembly test { }");
@@ -31,6 +37,36 @@ namespace Revn
                     writer.WriteLine(IL);
                 }
             }
+
+
+            var commandProcess = new Process
+                {
+                    StartInfo =
+                    {
+                        FileName = Environment.GetEnvironmentVariable( "ComSpec" ),
+                        CreateNoWindow = true,
+                        Arguments = $"/c ilasm \"{ilPath}\""
+                    }
+                };
+            commandProcess.Start();
+            commandProcess.WaitForExit();
+
+            Console.WriteLine("Execution Result");
+            Console.WriteLine("=======================================");
+
+            var executionPath = Path.Combine( location, "HelloWorld.exe" );
+            var ilProcess = new Process
+            {
+                StartInfo =
+                {
+                    FileName = executionPath,
+                    Arguments = "/c"
+                }
+            };
+            ilProcess.Start();
+            ilProcess.WaitForExit();
+
+            Console.ReadLine();
         }
     }
 }
